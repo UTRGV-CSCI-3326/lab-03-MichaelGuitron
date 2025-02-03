5b781fdb29924d2adeea7bb5eb8a7c4d7228b41b
MichaelGuitron <150745927+MichaelGuitron@users.noreply.github.com>
2025-02-03 05:32:18 +0000

    commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,18 @@
-
+public class BankAccount {
+    public static void main(String[] args) {
+        // Initial balance
+        double balance = 2175.37;
+        
+        // Transactions
+        balance -= 302.50; // Withdrawal
+        balance += 50.03;  // Deposit
+        balance -= balance / 2; // Withdraw half
+        balance += 20.00;  // Deposit
+        balance -= 1.00;   // Withdrawal
+        balance *= 2;      // Paycheck deposit (double balance)
+        balance += 1.00;   // Deposit
+        
+        // Output final balance
+        System.out.printf("Bob's new balance: $%.2f\n", balance);
+    }
+}
